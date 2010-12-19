6712641cf02252beb62194e9fa280faea5d8eb51
Christopher Lais <chris+android@zenthought.org>
2010-12-19 14:19:27 -0600

    glacier: fix bluetooth
    Switch to Vision bcm4329 firmware (Glacier has to be twiddled by
    HTC's proprietary btld daemon).
    
    Switch away from PURE_ANDROID, since it causes lockups on enabled/disable
    on Glacier.
    
    Change-Id: I869fcfc5f359519921d4d2c16f322eb8711db4dd

--- glacier.mk
+++ glacier.mk
@@ -86,7 +86,7 @@ PRODUCT_COPY_FILES += \
 
 # Firmware
 PRODUCT_COPY_FILES += \
-    device/htc/glacier/firmware/BCM4329B1_002.002.023.0511.0538.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/glacier/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
     device/htc/glacier/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
     device/htc/glacier/firmware/default.acdb:system/etc/firmware/default.acdb \
     device/htc/glacier/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
