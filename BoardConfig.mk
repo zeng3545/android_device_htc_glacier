464bcffb863d426d9507b12384579992aeaaa147
Nick Reuter <nreuter85@gmail.com>
2012-10-05 19:00:22 -0500

    kernel source already defined in msm7x30-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,6 @@ BOARD_KERNEL_PAGE_SIZE := 4096
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := glacier
 
 # Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_glacier_defconfig
 
 TARGET_USERIMAGES_USE_EXT4 := true
